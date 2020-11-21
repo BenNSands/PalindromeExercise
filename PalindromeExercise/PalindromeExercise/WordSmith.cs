24bd222ac824b521e780ccba10634dfabffbeaf8
Ben Sands <m3gap0ky0@gmail.com>
2020-11-21 15:05:07 -0700

    finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            string lowerCase = word.ToLower();
+            var charArr = lowerCase.ToCharArray();
+            var reverse = charArr.Reverse();
+            bool isPalindrome = lowerCase.SequenceEqual(reverse);
+            return isPalindrome;
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+using System.Linq;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("dad", true)]
+        [InlineData("god", false)]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("RaceCar", true)]
+        public void IsAPalindrome(string word, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
